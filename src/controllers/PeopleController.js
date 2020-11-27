6311956c8258210beacbc7e2f164fea1d04a2b7e
Vicente Avelino <vic5ente@gmail.com>
2020-11-27 16:53:34 -0300

    add update people

--- src/controllers/PeopleController.js
+++ src/controllers/PeopleController.js
@@ -21,7 +21,7 @@ module.exports = {
 
          await schema.validate(data, {
             abortEarly: false,
-         })
+         });
       
          const [id] = await connection('people').insert({name, gender, email, birthday, place_of_birthday, nationality, cpf });
 
@@ -30,7 +30,42 @@ module.exports = {
          if (error instanceof yup.ValidationError) {
             return res.json({ errors: error.errors});
          }
-         
+
+         return res.json(error);
+      }
+   },
+
+   async update(req, res) {
+      const { id } = req.params;
+      
+      const {name, gender, email, birthday, place_of_birthday, nationality, cpf } = req.body;
+
+      const data = {name, gender, email, birthday, place_of_birthday, nationality, cpf };
+
+      const schema = yup.object().shape({
+         name: yup.string().required(),
+         gender: yup.string(),
+         email: yup.string().email(),
+         birthday: yup.date().required(),
+         place_of_birthday: yup.string(),
+         nationality: yup.string(),
+         cpf: yup.string().length(11),
+      });
+
+      try {
+         await schema.validate(data, {
+            abortEarly: false,
+         });
+
+         const response = await connection('people').where('id', id).update(data);
+
+         return res.json({ status: !!response });
+
+      } catch(error) {
+         if (error instanceof yup.ValidationError) {
+            return res.json({ errors: error.errors});
+         }
+
          return res.json(error);
       }
    }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const PeopleController = require('./controllers/PeopleController');
 const routes = express.Router();
 
 routes.post('/people', PeopleController.create);
+routes.put('/people/:id', PeopleController.update);
 
 module.exports = routes;
